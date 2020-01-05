013e7d47c0ec1137d8bdaacd79fd51144048f992
LiviaMaria21 <liviambordea@gmail.com>
2020-01-05 20:22:41 +0200

    save name for outfit works

--- OutfitPlannerFrontend/src/app/choose-outfits/choose-outfits.page.ts
+++ OutfitPlannerFrontend/src/app/choose-outfits/choose-outfits.page.ts
@@ -1,55 +1,85 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, NavigationExtras, Router} from "@angular/router";
 import {Outfit} from "../interfaces/Outfit";
 import {Clothing} from "../interfaces/Clothing";
 import {OutfitService} from "../services/outfit.service";
 import {Category} from "../interfaces/Category";
+import {AlertController} from "@ionic/angular";
 
 
 @Component({
-  selector: 'app-choose-outfits',
-  templateUrl: './choose-outfits.page.html',
-  styleUrls: ['./choose-outfits.page.scss'],
+    selector: 'app-choose-outfits',
+    templateUrl: './choose-outfits.page.html',
+    styleUrls: ['./choose-outfits.page.scss'],
 })
 export class ChooseOutfitsPage implements OnInit {
 
-  outfits: Outfit[];
-  clothingId: Clothing;
-  categoryId: Category;
-  errorText: string;
+    outfits: Outfit[];
+    clothingId: Clothing;
+    categoryId: Category;
+    errorText: string;
 
-  constructor(private outfitService: OutfitService,private route: ActivatedRoute, private router: Router
-  ) {
-    this.route.queryParams.subscribe(params => {
-      if (this.router.getCurrentNavigation().extras.state) {
-        this.clothingId = this.router.getCurrentNavigation().extras.state.clothingId;
-        this.categoryId = this.router.getCurrentNavigation().extras.state.categoryId;
 
-      }
-    });
-  }
-  ngOnInit() {
-    this.outfitService.generateOutfit(this.clothingId,this.categoryId).subscribe(data=>{
-      this.outfits = data as Outfit[];
-    },error1 => {
-      this.errorText = error1.error;
-    })
-  }
+    constructor(private outfitService: OutfitService, private route: ActivatedRoute, private router: Router, private alertCtrl: AlertController
+    ) {
+        this.route.queryParams.subscribe(params => {
+            if (this.router.getCurrentNavigation().extras.state) {
+                this.clothingId = this.router.getCurrentNavigation().extras.state.clothingId;
+                this.categoryId = this.router.getCurrentNavigation().extras.state.categoryId;
 
-  goToClothingPage(clothing: Clothing) {
-    let navigationExtras: NavigationExtras = {
-      state: {
-        clothing: clothing
-      }
-    };
-    this.router.navigate(['/clothing'],navigationExtras)
-  }
+            }
+        });
+    }
 
-  save(outfit: Outfit) {
-    this.outfitService.save(outfit).subscribe((data)=>{
-      alert("Outfit successfully saved!");
-    },error1 => {
-      alert("Could not save outfit!");
-    });
-  }
+    async ngOnInit() {
+        await this.outfitService.generateOutfit(this.clothingId, this.categoryId).subscribe(data => {
+            this.outfits = data as Outfit[];
+        }, error1 => {
+            this.errorText = error1.error;
+        })
+    }
+
+    goToClothingPage(clothing: Clothing) {
+        let navigationExtras: NavigationExtras = {
+            state: {
+                clothing: clothing
+            }
+        };
+        this.router.navigate(['/clothing'], navigationExtras)
+    }
+
+    async save(outfit: Outfit) {
+        let alert = await this.alertCtrl.create({
+            header: 'Save outfit',
+            inputs: [
+                {
+                    name: 'outfitName',
+                    placeholder: 'Outfit name'
+                }
+            ],
+            buttons: [
+                {
+                    text: 'Cancel',
+                    role: 'cancel',
+                    handler: () => {
+                        console.log('Cancel clicked');
+                    }
+                },
+                {
+                    text: 'Save',
+                    handler: (data) => {
+
+                        console.log(data);
+                        outfit.name = data.outfitName;
+                        this.outfitService.save(outfit).subscribe((response) => {
+                            console.log(response)
+                        }, error1 => {
+                            console.log(error1);
+                        });
+                    }
+                }
+            ]
+        });
+        alert.present();
+    }
 }
